cc1bda2e721b7e6cb0494ac3acee61b9eea6ff72
Timii <timiiowolabi@gmail.com>
2024-12-10 17:32:02 +0100

    Add files via upload

---
+++ formation_flight.py
@@ -0,0 +1,27 @@
+import time
+
+import numpy as np
+import pybullet as p
+import matplotlib.pyplot as plt
+import pybullet_data
+
+from gym_pybullet_drones.utils.enums import DroneModel, Physics
+from gym_pybullet_drones.envs.CtrlAviary import CtrlAviary
+from gym_pybullet_drones.control.DSLPIDControl import DSLPIDControl
+from gym_pybullet_drones.utils.Logger import Logger
+from gym_pybullet_drones.utils.utils import sync, str2bool
+
+DEFAULT_DRONES = DroneModel("cf2x")
+DEFAULT_NUM_DRONES = 3
+DEFAULT_PHYSICS = Physics("pyb")
+DEFAULT_GUI = True
+DEFAULT_RECORD_VISION = False
+DEFAULT_PLOT = True
+DEFAULT_USER_DEBUG_GUI = False
+DEFAULT_OBSTACLES = True
+DEFAULT_SIMULATION_FREQ_HZ = 240
+DEFAULT_CONTROL_FREQ_HZ = 48
+DEFAULT_DURATION_SEC = 12
+DEFAULT_OUTPUT_FOLDER = 'results'
+DEFAULT_COLAB = False
+
---
+++ minimum_jerk_trajectory.py
@@ -0,0 +1,3 @@
+# Generate the position, velocity, (acceleration, jerk, snap)* profiles
+# Take into account the model of a quadrotor
+# Use a position or velocity controller to track the trajectory
